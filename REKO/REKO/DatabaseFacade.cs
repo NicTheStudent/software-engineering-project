d1bb17e769c00a3659b1f5d183162761b4a55524
osgro19 <o.gronqvist@telia.com>
2019-04-15 16:10:43 +0200

    La till en Offer-klass samt en facad för att samla instanser of Offer

---
+++ REKO/REKO/DatabaseFacade.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Foundation;
+
+namespace REKO
+{
+    public class DatabaseFacade
+    {
+
+        List<Offer> offerList = new List<Offer>();
+
+        public DatabaseFacade()
+        {
+            offerList.Add(new Offer("Eggberts Ägg", 40, "E.L. Eggbert", 144, 0, "dussin", true));
+            offerList.Add(new Offer("Bertils betor", 10, "Bertil Knutsson", 20, 0, "kg", true));
+            offerList.Add(new Offer("Grönqvists gröna gurkor", 30, "Oskar Grönqvist", 15, 0, "st.", true));
+            offerList.Add(new Offer("Marias margarin", 50, "Maria", 1000, 0, "g", true));
+            offerList.Add(new Offer("Hampus & Sampas hampa", 50, "Hampus & Samuel", 300, 0, "g", true));
+        }
+
+    }
+}
---
+++ REKO/REKO/Offer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace REKO
+{
+    public class Offer
+    {
+
+        String name, seller, unit;
+        double price;
+        int available, ordered;
+        bool published;
+
+        public Offer(String name, double price, String seller, int available, int ordered, String unit, bool published)
+        {
+            this.name = name;
+            this.price = price;
+            this.seller = seller;
+            this.available = available;
+            this.ordered = ordered;
+            this.unit = unit;
+            this.published = published;
+        }
+
+        public String GetName()
+        {
+            return name;
+        }
+        public double GetPrice()
+        {
+            return price;
+        }
+        public String GetPriceString()
+        {
+            return String.Format("{0:F2}", price);
+        }
+        public String GetSeller()
+        {
+            return seller;
+        }
+        public int GetAvailable()
+        {
+            return available;
+        }
+        public int GetOrdered()
+        {
+            return ordered;
+        }
+        public String GetUnit()
+        {
+            return unit;
+        }
+        public bool GetPublished()
+        {
+            return published;
+        }
+
+        //----------------------------------
+
+
+
+    }
+}
