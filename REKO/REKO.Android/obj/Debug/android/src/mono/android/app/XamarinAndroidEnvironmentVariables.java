03e199a93263bf68e276323b4b6f3da84002afdb
Lucas Andrén <lucasa@live.se>
2019-05-06 14:02:00 +0200

    Add a login screen as the new first page

--- REKO/REKO.Android/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ REKO/REKO.Android/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "134bc89f-8e7f-489e-b251-c379cfdde199",
+		"XAMARIN_BUILD_ID", "242c26db-e118-433d-95e2-89a3f9cc4fa9",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
---
+++ REKO/REKO/classes/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace REKO.classes
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User(string Username, string Password)
+        {
+            this.Username = Username;
+            this.Password = Password;
+
+        }
+
+        public bool LoginValidation()
+        {
+            if (!this.Username.Equals(" ") && !this.Password.Equals(" "))
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
+
