7229c62ee3e72726a7b04d93ffe3deb7343138c8
osgro19 <o.gronqvist@telia.com>
2019-05-13 10:50:46 +0200

    added newoffer and accounttab

---
+++ REKO/REKO.Android/NewOffer.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace REKO.Droid
+{
+    public partial class NewOffer : ContentPage
+    {
+        public NewOffer()
+        {
+            InitializeComponent();
+        }
+    }
+}
