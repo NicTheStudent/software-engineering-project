5ab1b38abb912c365e16a0c6144b069b87856f4a
Hampus Broman <namorb@outlook.com>
2019-05-19 19:56:25 +0200

    går  också att komma till beställ sidan från producertab nu

--- REKO/REKO/ProducerPage.xaml.cs
+++ REKO/REKO/ProducerPage.xaml.cs
@@ -20,7 +20,12 @@ namespace REKO
          MainListView.ItemsSource = DatabaseFacade.Instance.GetOffers(Producer);
         }
 
-        
-
+        async private void MainListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var Selected = e.Item as Offer;
+            OfferDetailed detailedPage = new OfferDetailed(Selected);
+            await Navigation.PushAsync(detailedPage);
+            ((ListView)sender).SelectedItem = null;
+        }
     }
 }
