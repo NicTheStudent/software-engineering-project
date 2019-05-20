70a2bd0d389b0c6cd9bcf45451b899eb72598c71
DecibelM <42909365+DecibelM@users.noreply.github.com>
2019-05-20 23:17:29 +0200

    Fixade hårdkodad layout för detailed order page. Rubrik och fina färger.

--- REKO/REKO/DetailedOrder.xaml.cs
+++ REKO/REKO/DetailedOrder.xaml.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+
+//TODO Fixa med table view.
+//TODO Kolla så all info är med.
 namespace REKO
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
