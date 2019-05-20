9b1735a21b1d312c6dd5ef15bdfeecd75692e62e
Lucas Andrén <lucasa@live.se>
2019-05-20 22:19:27 +0200

    Redirects when an order is placed, if you're not logged in

--- REKO/REKO/OfferDetailed.xaml.cs
+++ REKO/REKO/OfferDetailed.xaml.cs
@@ -24,30 +24,37 @@ namespace REKO
 
         public void Handle_Clicked_Place_My_Order(object sender, EventArgs e)
         {
-
-            if (NrOfItems.Text == null)
+            if (Session.Instance.IsLoggedIn())
             {
-                DisplayAlert("Felaktig beställning", "Kontrollera att du fyllt i alla fält korrekt", "OK");
+                if (NrOfItems.Text == null)
+                {
+                    DisplayAlert("Felaktig beställning", "Kontrollera att du fyllt i alla fält korrekt", "OK");
+                }
+                else
+                {
+
+                    var amount = int.Parse(NrOfItems.Text);
+
+                    DatabaseFacade db = DatabaseFacade.Instance;
+                    var orderNumber = NextOrderNumber(db.GetOrders());  //Checks the hidghest orderNr in the database and gives the next 
+                    Order newOrder = new Order(Session.Instance.GetUser(), Offer, orderNumber, amount);
+
+                    DisplayAlert("Din beställning har lagts!", "Tack för din beställning av " + amount + " " + Offer.Product +
+                                 "\nDitt ordernummer är: " + orderNumber, "OK");
+
+                    db.AddOrder(newOrder);
+                }
             }
             else
             {
-
-               var amount = int.Parse(NrOfItems.Text);
-
-                DatabaseFacade db = DatabaseFacade.Instance;
-                var orderNumber = nextOrderNumber(db.GetOrders());  //Checks the hidghest orderNr in the database and gives the next 
-                Order newOrder = new Order(Session.Instance.GetUser(), Offer, orderNumber, amount);
-
-                DisplayAlert("Din beställning har lagts!", "Tack för din beställning av " + amount + " " + Offer.Product +
-                             "\nDitt ordernummer är: " + orderNumber, "OK");
-
-                db.AddOrder(newOrder);
+            RedirectOnInfo();
+               
             }
         }
         /*
          * Looks through all the orders in the DB and returns the highest+1
          */
-        private int nextOrderNumber(List<Order> list)
+        private int NextOrderNumber(List<Order> list)
         {
             int tempNr = 0;
             foreach(Order item in list)
@@ -56,6 +63,19 @@ namespace REKO
                     tempNr = item.orderNumber;
             }
             return tempNr + 1;
+        }
+      
+        public async void RedirectOnInfo()
+        {
+            string answer = await DisplayActionSheet("Inloggning krävs", "Avbryt", null, "Logga in", "Skapa konto");
+
+            if(answer.Equals("Logga in")) {
+               await Navigation.PushAsync(new LoginPage());
+            }
+            else if (answer.Equals("Skapa konto"))
+            {
+                await Navigation.PushAsync(new SignUpPage());
+            }
         }
 	}
 }
