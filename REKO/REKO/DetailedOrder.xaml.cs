db660d6062cb3d30bfe84ab36ba2a272ed2ee6d7
DecibelM <42909365+DecibelM@users.noreply.github.com>
2019-05-21 22:44:56 +0200

    Tog bort bös

--- REKO/REKO/DetailedOrder.xaml.cs
+++ REKO/REKO/DetailedOrder.xaml.cs
@@ -4,9 +4,6 @@ using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-
-//TODO Fixa med table view.
-//TODO Kolla så all info är med.
 namespace REKO
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
